170513c52827f196ab1833b542fa750189cef5c2
Ibrahimelsayed60 <hemasayed600@gmail.com>
2021-05-23 00:52:04 +0200

    new

---
+++ main.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtGui import QPixmap,QImage
+from PyQt5.QtCore import Qt
+from task import Ui_MainWindow
+import sys
+import math
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
