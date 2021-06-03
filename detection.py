f455004973d789046f12fd55240e06ce06458208
Donia-sharafeldin <donia.ali98@eng-st.cu.edu.eg>
2021-06-03 03:19:50 +0200

    detection

---
+++ detection.py
@@ -0,0 +1,20 @@
+import face_recognition
+from PIL import Image, ImageDraw
+import cv2
+face = cv2.CascadeClassifier('cascade\haarcascade_frontalface_alt2.xml')
+img = cv2.imread('data\image_050.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+faces = face.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=5)
+
+for (x,y,w,h)in faces:
+    print(x,y,w,h)
+    roi = gray[y: y+h, x:x+w]
+    # imgitm = "det.png"
+    # cv2.imwrite(imgitm, roi)
+    color = (255,0,0)
+    stroke = 2
+    width = x+w
+    height = y+h
+    cv2.rectangle(img,(x,y),(width, height),color, stroke)
+cv2.imwrite('img.png',img)
+cv2.waitKey(0)
